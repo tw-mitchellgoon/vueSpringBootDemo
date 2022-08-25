babe2ffe35c8e902eb271e92c4b459d349396b85
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-08-25 11:54:52 +0800

    Remove HelloWorld from App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ToDo />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from "./components/ToDoPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    ToDo,
+  },
+};
 </script>
 
 <style>

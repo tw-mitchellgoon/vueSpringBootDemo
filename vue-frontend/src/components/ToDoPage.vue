48b6a0b0233c74de0200d4f658903640312d82cd
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-08-25 11:54:15 +0800

    Add ToDo component

---
+++ vue-frontend/src/components/ToDoPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <ul>
+      <li>
+        <span
+          ><p>Task 1</p>
+          <input type="checkbox" />
+        </span>
+      </li>
+      <li>
+        <span
+          ><p>Task 2</p>
+          <input type="checkbox" />
+        </span>
+      </li>
+      <li>
+        <span
+          ><p>Task 3</p>
+          <input type="checkbox" />
+        </span>
+      </li>
+    </ul>
+    <button>Add Task</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+  setup() {},
+};
+</script>
+
+<style scoped></style>

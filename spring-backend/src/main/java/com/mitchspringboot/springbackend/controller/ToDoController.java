a579712b77b402a9b5b12ab27f02285729941248
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-08-29 19:23:34 +0800

    Add POST request for adding to do item

--- spring-backend/src/main/java/com/mitchspringboot/springbackend/controller/ToDoController.java
+++ spring-backend/src/main/java/com/mitchspringboot/springbackend/controller/ToDoController.java
@@ -3,7 +3,12 @@ package com.mitchspringboot.springbackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +24,12 @@ public class ToDoController {
     @GetMapping("/todolist")
     public List<ToDo> fetchToDo() {
         return toDoRepository.findAll();
+    }
+
+    @PostMapping(path = "/todoadd", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ToDo> addToDo(@RequestBody ToDo newToDo) throws Exception {
+        toDoRepository.ToDoAdd(newToDo.getTitle(), false);
+
+        return new ResponseEntity<ToDo>(newToDo, HttpStatus.CREATED);
     }
 }
--- spring-backend/src/main/java/com/mitchspringboot/springbackend/repository/ToDoRepository.java
+++ spring-backend/src/main/java/com/mitchspringboot/springbackend/repository/ToDoRepository.java
@@ -3,11 +3,20 @@ package com.mitchspringboot.springbackend.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.mitchspringboot.springbackend.model.ToDo;
 
 public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
     @Query(nativeQuery = true, value = "SELECT * FROM todolist ")
     List<ToDo> ToDoList();
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true, value = "INSERT INTO todolist (title, completed) values (:title, :completed)")
+    void ToDoAdd(@Param("title") String title,
+            @Param("completed") Boolean completed);
 }

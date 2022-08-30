3ea1ba9de804cd3a55cfaf26f15c546fdf0eaa22
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-08-30 11:55:42 +0800

    Add POST request for adding todo item to backend

--- vue-frontend/src/services/ToDoService.js
+++ vue-frontend/src/services/ToDoService.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
-const TODO_API_BASE_URL =
-  "http://localhost:8080/api/todolist";
+const TODO_API_BASE_URL = "http://localhost:8080/api/";
 
 class ToDoService {
   getToDoList() {
-    return axios.get(TODO_API_BASE_URL);
+    return axios.get(`${TODO_API_BASE_URL}todolist`);
+  }
+
+  addToDo(toDoItem) {
+    return axios
+      .post(`${TODO_API_BASE_URL}todoadd`, toDoItem)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
   }
 }
 

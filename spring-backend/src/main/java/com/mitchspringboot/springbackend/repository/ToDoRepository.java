bf342a4833e20938b1e8003bacc34e3c388c5306
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-08-31 17:43:03 +0800

    Refactor ToDo Repo

--- spring-backend/src/main/java/com/mitchspringboot/springbackend/repository/ToDoRepository.java
+++ spring-backend/src/main/java/com/mitchspringboot/springbackend/repository/ToDoRepository.java
@@ -17,6 +17,7 @@ public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
     @Transactional
     @Modifying
     @Query(nativeQuery = true, value = "INSERT INTO todolist (title, completed) values (:title, :completed)")
-    void ToDoAdd(@Param("title") String title,
+    void ToDoAdd(
+            @Param("title") String title,
             @Param("completed") Boolean completed);
 }

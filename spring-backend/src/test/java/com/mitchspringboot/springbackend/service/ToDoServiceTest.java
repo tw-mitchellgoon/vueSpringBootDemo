28abed9e315b3a31e0ffe4c2029cf0c35c1df004
Mitchell Goon <mitchell.goon@thoughtworks.com>
2022-09-06 17:39:39 +0800

    Add test for fetching all todo items service

---
+++ spring-backend/src/test/java/com/mitchspringboot/springbackend/service/ToDoServiceTest.java
@@ -0,0 +1,42 @@
+package com.mitchspringboot.springbackend.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.mitchspringboot.springbackend.model.ToDo;
+import com.mitchspringboot.springbackend.repository.ToDoRepository;
+
+@ExtendWith(SpringExtension.class)
+public class ToDoServiceTest {
+
+    @Mock
+    ToDoRepository toDoRepository;
+
+    @InjectMocks
+    ToDoService toDoService;
+
+    private final Integer id = (int) Math.random() * 100;
+    private final String title = "Task " + id;
+    private final Boolean completed = false;
+
+    private final ToDo expectedToDo = new ToDo(id, title, completed);
+
+    @Test
+    public void shouldGetAllToDoItems() {
+        when(toDoRepository.ToDoList()).thenReturn(List.of(expectedToDo));
+
+        List<ToDo> toDoList = toDoService.getAllToDo();
+        assertEquals(1, toDoList.size());
+        assertEquals(id, toDoList.get(0).getId());
+        assertEquals(title, toDoList.get(0).getTitle());
+        assertEquals(completed, toDoList.get(0).getCompleted());
+    }
+}
